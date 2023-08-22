090455a2409ed8c9fc7fdfc1698cc4b7c2673c7f
krmpss <142886858+krmpss@users.noreply.github.com>
2023-08-22 16:44:31 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,24 +1,32 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Lighting
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(1, 1, 1);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+
+const light = new THREE.AmbientLight(0x888888);
 scene.add(light);
 
-// Camera position
-camera.position.z = 5;
+const directionalLight = new THREE.DirectionalLight(0xffffff);
+directionalLight.position.set(1, 1.5, 1);
+scene.add(directionalLight);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-
 let cubes = [];
 
+camera.position.z = 60;
+camera.position.y = 30;
+
+// Grid helper for better orientation
+const gridHelper = new THREE.GridHelper(50, 50);
+scene.add(gridHelper);
+
 document.addEventListener('click', function(event) {
-    // Get clicked position in the 3D space
     const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
     const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
 
@@ -35,7 +43,7 @@ document.addEventListener('click', function(event) {
     } else {
         // Add a cube
         const cube = new THREE.Mesh(geometry, material);
-        cube.position.set(Math.floor(vector.x), Math.floor(vector.y), Math.floor(vector.z));
+        cube.position.set(Math.round(vector.x), Math.round(vector.y), Math.round(vector.z));
         cubes.push(cube);
         scene.add(cube);
     }
@@ -43,6 +51,7 @@ document.addEventListener('click', function(event) {
 
 function animate() {
     requestAnimationFrame(animate);
+    controls.update();
     renderer.render(scene, camera);
 }
 
