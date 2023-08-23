4395fe476593c14af0e70a2c242514c698eb05fe
krmpss <142886858+krmpss@users.noreply.github.com>
2023-08-23 10:13:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 function redirectUser() {
-    window.location.href = 'https://www.google.com/search?q=first+aid+kit&sca_esv=559317661&tbm=isch&sxsrf=AB5stBi2AkgjH-GY4e_Ti8kbwbwgCRAIfw:1692772876667&source=lnms&sa=X&ved=2ahUKEwjV4f6vlvKAAxWGJcAKHU14BlIQ_AUoAnoECAUQBA&biw=1745&bih=910&dpr=1.1';
+    window.location.href = 'https://www.yourcustomlink.com';
 }
 
-document.getElementById('scrolling-text-content').textContent = 'Do you dare to come to the party?';
-document.getElementById('tvVideo').play();
-
-let videoElement = document.getElementById('tvVideo');
-videoElement.setAttribute('loop', 'true');
-videoElement.muted = true;  // this mutes the video, remove if you want sound
-let videoElement = document.getElementById('tvVideo');
-videoElement.play().catch(error => {
-    // Auto-play was prevented
-    // Show a UI element to let the user manually start playback
-});
+document.getElementById('scrolling-text-content').textContent = 'Your 20 words text goes here...';
