ebd987c66c37abef75c26d3fa61cecdba24b5fb5
krmpss <142886858+krmpss@users.noreply.github.com>
2023-08-23 09:41:39 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function redirectUser() {
+    window.location.href = 'https://www.google.com/search?q=first+aid+kit&sca_esv=559317661&tbm=isch&sxsrf=AB5stBi2AkgjH-GY4e_Ti8kbwbwgCRAIfw:1692772876667&source=lnms&sa=X&ved=2ahUKEwjV4f6vlvKAAxWGJcAKHU14BlIQ_AUoAnoECAUQBA&biw=1745&bih=910&dpr=1.1';
+}
+
+document.getElementById('scrolling-text-content').textContent = 'Do you dare to come to the party?';
